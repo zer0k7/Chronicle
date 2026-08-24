1a12ae324561e4824f28bbde15c5b9cf2c08066d
zer0k7 <whoami@mailchat.me>
2026-08-24 13:20:27 +0530

    release: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.chronicle.usagestats"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
