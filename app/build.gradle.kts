6a35f623cf8af6a0f90a5a4b81d8e08b12ec2dd9
zer0k7 <whoami@mailchat.me>
2026-08-25 21:01:13 +0530

    chore(release): bump version to 1.0.8 (code 9) with automated tag-based release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.chronicle.usagestats"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
